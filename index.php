d976616781f3282a03b78cc74a53f5a29ddbe318
celsoishii <celsoyi@uol.com.br>
2021-02-04 15:35:54 -0300

    4 commit

--- index.php
+++ index.php
@@ -3,15 +3,26 @@
 <h3> Um deploy automático feito com alunos do itaú </h3>
 
 <div style = "display:flex">
-<div style="width:200px;height:200px;background:yellow;padding:50px;margin:10px">
+<div style="width:200px;height:400px;background:yellow;padding:50px;margin:10px">
   Bicleta 1 a venda
    <br>
   <img src="b1.jpg" style="width: 200px">
-</div>
+  <br>
+  R$ 9.500,00
+  <br><br>
+    <a href="https://pag.ae/7WRynf6X1" target="_blank" style="display:block;background:green;color:white;text-align:center;padding:20px;border-radius:10px">Comparar agora</a><br>
+	<a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" >Entrar em contato com o vendedor</a>
+  </div>
 
-<div style="width:200px;height:200px;background:blue;color:white;padding:50px;margin:10px">
+<div style="width:200px;height:400px;background:blue;color:white;padding:50px;margin:10px">
   Bicicleta 2 a venda
   <br>
   <img src="b2.jpg" style="width: 200px">
+  <br>
+  R$ 8.500,00
+  <br><br>
+    <a href="https://pag.ae/7WRynf6X1" target="_blank" style="display:block;background:green;color:white;text-align:center;padding:20px;border-radius:10px">Comparar agora</a><br>
+	<a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" style="color:white">Entrar em contato com o vendedor</a>
+  </div>
 </div>
 </div>
