9b975bf13cf9e44ad50f15b2e0e058259b56eee2
celsoishii <celsoyi@uol.com.br>
2021-02-04 14:55:32 -0300

    segundo commit

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <h1>Bem vindo turma 14A do itaú pelo treinamento da Gama </h1>
 
-<h3> Um deploy aotomático feito com alunos do itaú </h3>
+<h3> Um deploy automático feito com alunos do itaú </h3>
 
-<h5>Tentar sempre, desistir jamais!!!</h5>
-
-<h1>app arrozcomfeijao</h1>
-
-<div style="width:200px;height:200px;background:yellow">
+<div style = "display:flex">
+<div style="width:200px;height:200px;background:yellow;padding:50px;margin:10px">
   Um teste em um quadrado
+</div>
+
+<div style="width:200px;height:200px;background:blue;color:white;padding:50px;margin:10px">
+  Um teste em um quadrado
+</div>
 </div>
